b91ea2f0787abf5c66632f1bf9c57510ca12b8a0
Surbi Karki <surbi.211740@ncit.edu.np>
2026-06-02 12:16:16 +0545

    Add solution for transformers-feed-forward

---
+++ transformer/transformers-feed-forward/transformers-feed-forward.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def feed_forward(x: np.ndarray, W1: np.ndarray, b1: np.ndarray,
+                 W2: np.ndarray, b2: np.ndarray) -> np.ndarray:
+    """
+    Apply position-wise feed-forward network.
+    """
+    #first linear layer
+    hidden = np.dot(x,W1)+b1
+
+    #ReLU activation
+    relu_out = np.maximum(0,hidden)
+    #second linear layer
+    output = np.dot(relu_out,W2)+b2
+
+    return output
+
+    
